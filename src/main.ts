aa1d3f30768472f0bb08c614bf59d1f9b2f259ef
Zaw Htet Naing <yourbiddaddy@gmail.com>
2024-07-16 23:43:28 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api')
+  const config = new DocumentBuilder()
+  .setTitle('Learning platform')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('doc',app,document)
+  await app.listen(3000);
+}
+bootstrap();
