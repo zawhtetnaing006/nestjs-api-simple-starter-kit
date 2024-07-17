084e031b24e569424c68da9c1c382600f6374546
Zaw <zawhtetnaing006@gmail.com>
2024-07-17 14:48:29 +0700

    [main] linting

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
-  .setTitle('Learning platform')
-  .setVersion('1.0')
-  .build();
-  const document = SwaggerModule.createDocument(app,config);
-  SwaggerModule.setup('doc',app,document)
+    .setTitle('Learning platform')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
