57a57946523fa6c72e6485d2d6762517313331e4
Zaw <zawhtetnaing006@gmail.com>
2024-07-18 17:46:31 +0700

    [main] add explanation about factory definitions

--- src/seeder/factory/factory.ts
+++ src/seeder/factory/factory.ts
@@ -1,5 +1,5 @@
 export interface Factory {
-  model: string;
-  count: number;
-  definition(): object;
+  model: string; //valide prisma client model name
+  count: number; //number of rows to seed
+  definition(): object; //must return valid prisma clientCreateInput
 }
